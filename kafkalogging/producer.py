568400da0679b56961d0abf86c6f5f91af5208b2
Hrishikesh <hr.telang@gmail.com>
2019-04-22 16:34:18 -0400

    Update producer.py

--- kafkalogging/producer.py
+++ kafkalogging/producer.py
@@ -1,4 +1,4 @@
 from kafka import KafkaProducer
 producer = KafkaProducer(bootstrap_servers='2181')
-producer.send('sample', b'Test')
-producer.send('sample', key=b'message-two', value=b'This is Test with key')
+producer.send('clientaccess', b'Test')
+producer.send('clientaccess', key=b'message-two', value=b'This is Test with key')
