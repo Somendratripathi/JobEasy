dbb787bc3caf4c80bfa293920c03d3dbe79927e9
Carlo Ruiz <carloruiz.57@gmail.com>
2019-04-29 14:32:47 -0400

    extractor writes to kafka

--- extractor/con.py
+++ extractor/con.py
@@ -7,12 +7,21 @@ import json
 from html import escape
 from json import loads
 
+kafka_producer = KafkaProducer(bootstrap_servers=['localhost:9092'], 
+    value_serializer=lambda x: dumps(x).encode('utf-8'))
+
 consumer1=KafkaConsumer('sample',value_deserializer=lambda x: loads(x.decode('utf-8')))
+
 #consumer2= KafkaConsumer('btest')
 
 for message in consumer1:
     #print(html)
     #print(type(html))
     print(message)
-    company, jobtitle, jobdescription =Extraction.extract_text(message.value)
+    company, jobtitle, jobdescription = Extraction.extract_text(message.value)
     print(company, jobtitle)
+    kafka_producer.send('query', value={
+        "company": company,
+        "jobtitle": jobtitle,
+        "jobdescription": jobdescription
+    })
