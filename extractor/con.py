9ed2b6c313286165fa199aca7d58577f30310d88
Hrishikesh <hr.telang@gmail.com>
2019-04-29 15:03:14 -0400

    Update con.py

--- extractor/con.py
+++ extractor/con.py
@@ -1,16 +1,16 @@
-from kafka import KafkaConsumer
+from kafka import KafkaConsumer,KafkaProducer
 from time import sleep
 from exctraction import Extraction
 from bs4 import BeautifulSoup
 import re
 import json
 from html import escape
-from json import loads
+from json import loads,dumps
 
 kafka_producer = KafkaProducer(bootstrap_servers=['localhost:9092'], 
     value_serializer=lambda x: dumps(x).encode('utf-8'))
 
-consumer1=KafkaConsumer('sample',value_deserializer=lambda x: loads(x.decode('utf-8')))
+consumer1=KafkaConsumer('sample',bootstrap_servers=['localhost:9092'],value_deserializer=lambda x: loads(x.decode('utf-8')))
 
 #consumer2= KafkaConsumer('btest')
 
@@ -18,7 +18,7 @@ for message in consumer1:
     #print(html)
     #print(type(html))
     print(message)
-    company, jobtitle, jobdescription = Extraction.extract_text(message.value)
+    company, jobtitle, jobdescription = Extraction.extract_text(message.value['html'])
     print(company, jobtitle)
     kafka_producer.send('query', value={
         "company": company,
