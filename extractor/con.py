974cc231444eb0afb61a8e070adcbda6be93aeb2
Hrishikesh <hr.telang@gmail.com>
2019-05-10 19:52:32 -0400

    Update con.py

--- extractor/con.py
+++ extractor/con.py
@@ -6,6 +6,10 @@ import re
 import json
 from html import escape
 from json import loads,dumps
+from cassandra.cluster import Cluster
+
+cluster = Cluster(['127.0.0.1'])
+session = cluster.connect('clientstore')
 
 kafka_producer = KafkaProducer(bootstrap_servers=['localhost:9092'], 
     value_serializer=lambda x: dumps(x).encode('utf-8'))
@@ -17,10 +21,14 @@ consumer1=KafkaConsumer('sample',bootstrap_servers=['localhost:9092'],value_dese
 for message in consumer1:
     print(message)
     company, jobtitle, jobdescription = Extraction.extract_text(message.value['html'])
+    u=message.value['uid']
+    print(u)
     print(company, jobtitle)
     kafka_producer.send('query', value={
         "company": company,
         "jobtitle": jobtitle,
         "jobdescription": jobdescription,
-        "uid": uid
+        "uid": u
     })
+    CQLString = "INSERT INTO logs (uid) VALUES (%s)"
+    session.execute(CQLString, (u,))
