89addb52c1235b2af05adef323c0506dcf689703
Somendra <somendra.dutt.tripathi@gmail.com>
2019-05-11 01:03:48 -0400

    simplified query cache removed pickling

--- query/cache_h1b.py
+++ query/cache_h1b.py
@@ -1,16 +1,21 @@
-import pickle
 import time 
 
+#No need for pickling
+cache = {}
 def query_cache(company_name, job_title):
-	with open(r"./cache.pkl", "rb") as input_file:
-			cache = pickle.load(input_file)
-	print(cache)
+	#with open(r"./cache.pkl", "rb") as input_file:
+	#		cache = pickle.load(input_file)
 
-	try:
-		score = cache[company_name+':'+job_title]
-		return score, True
-	except KeyError as e:
+	#print(cache)
+	if cache.get(None,[company_name+':'+job_title]) == None:
 		return 0, False
+	
+	score = cache[company_name+':'+job_title]
+	#try:
+		
+	return score, True
+	#except KeyError as e:
+	#	return 0, False
 
 def add_cache(company_name, job_title, score, timestamp):
 	# current time
